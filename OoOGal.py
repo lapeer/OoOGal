084e0015ba43b1b7346893e871b3b78c5c233332
Drew <dlapeer@umass.edu>
2026-06-22 13:52:46 -0700

    adding outline

--- OoOGal.py
+++ OoOGal.py
@@ -6,3 +6,26 @@ class Galaxy(object):
     Galaxy class.
     
     """
+
+    def __init__(self, name = None, right_ascension = None, 
+                 declination = None):
+        
+        self.name = name 
+
+        self.RA = right_ascension 
+
+        self.Dec = declination 
+
+        return 
+
+
+    def init_galaxy_from_dict(self):
+
+        
+        '''
+        Initalize a Galaxy object based on a list of properties 
+        '''
+
+        return 
+
+        
