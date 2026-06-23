8371cf009ec8e5285007c071b641e18abcd3fe6d
Kesha <kesha.patel@tufts.edu>
2026-06-23 15:36:02 -0700

    updated after kesha

--- MAST.py
+++ MAST.py
@@ -1,10 +1,13 @@
 import numpy as np 
 from astroquery.mast import Observations
 from astroquery.mast import Mast
+from astroquery.mast import MastMissions
 
 
 from OoOGal import * 
 
+HST_instruments = ['ACS', 'WFPC', 'WFC3', 'WFPC2', 'FOC', 'HSP' ]
+JWST_instruments = ['NIRCAM']
 
 def do_query_mast(Galaxy):
 
@@ -19,15 +22,37 @@ def do_query_mast(Galaxy):
     
     #get the coords of the object 
     try:
+        #first try to search by name 
         coords = mast.resolve_object(name, resolve_all=False)
     except:
+        #then try to search by coordinates if name fails 
         coords = Galaxy.get_coords()
 
+    results_arr = []
+
+    #obtaining available photometric data for HST and JWST instruments 
+    for telescope, instrument in zip(['HST', 'JWST'], [HST_instruments, JWST_instruments]):
+
+        missions = MastMissions(mission = telescope)
+
+        if telescope == 'HST':
+            select_cols = ['sci_data_set_name', 'sci_instrume', 'sci_aper_1234', ' sci_spec_1234', 'sci_actual_duration', 'sci_central_wavelength']
+            results = missions.query_object(name, sci_instrume = instrument)
+            
+            
+        else: 
+            select_cols = ['instrume', 'opticalElements', 'duration', 'program']
+            results = missions.query_object(name, instrume = instrument)
+
+        results_arr.append(results)
+        
+
+        instru_list = results['']
 
 
-    #first try to search by name 
 
-    #then try to search by coordinates if name fails 
+
+    
 
 
 
