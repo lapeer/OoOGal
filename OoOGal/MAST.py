9134b3d8c8f4956c6156e01b2faee103e1c17201
Kesha <kesha.patel@tufts.edu>
2026-06-25 20:05:32 -0700

    adding .txt

--- OoOGal/MAST.py
+++ OoOGal/MAST.py
@@ -1,5 +1,4 @@
 import numpy as np
-from astroquery.mast import Observations
 from astroquery.mast import Mast
 from astroquery.mast import MastMissions
 import astropy.table
@@ -7,15 +6,16 @@ import astropy
 
 from OoOGal import *
 
+# instruments that provide photometric data
 HST_instruments = ["ACS", "WFPC", "WFC3", "WFPC2", "FOC", "HSP"]
-JWST_instruments = ["NIRCAM", 'MIRI']
+JWST_instruments = ["NIRCAM", "MIRI"]
 
 
 def do_query_mast(Galaxy):
     """
     Query MAST
 
-    Perform MAST search for an inputted Galaxy object. Searches for all available HST and JWST data.
+    Perform MAST search for an inputted Galaxy object. Searches for all available HST and JWST photometric data.
 
     Args:
         Galaxy (object): Inputted galaxy
@@ -62,18 +62,24 @@ def do_query_mast(Galaxy):
                 name, instrume=instrument, select_cols=select_cols
             )
 
-
         results_arr.append(results)
 
-    for res, cols in zip(results_arr, 
-                         [["sci_instrume","sci_spec_1234", "sci_pep_id",], 
-                         ["instrume", "opticalElements", "program"]]):
-        columns = np.array(res.colnames )
+    for res, cols in zip(
+        results_arr,
+        [
+            [
+                "sci_instrume",
+                "sci_spec_1234",
+                "sci_pep_id",
+            ],
+            ["instrume", "opticalElements", "program"],
+        ],
+    ):
+        columns = np.array(res.colnames)
         mask_remove = [False if i in cols else True for i in columns]
-        
-        columns_to_remove = columns[mask_remove]
-        res.remove_columns(columns_to_remove) 
 
+        columns_to_remove = columns[mask_remove]
+        res.remove_columns(columns_to_remove)
 
     # renaming column names to more comprehensible headers
     hst_results = results_arr[0]
@@ -81,24 +87,25 @@ def do_query_mast(Galaxy):
 
     hst_results.rename_column("sci_instrume", "instrument")
     hst_results.rename_column("sci_spec_1234", "filters")
-    #hst_results.rename_column("sci_actual_duration", "exposure time")
+    # hst_results.rename_column("sci_actual_duration", "exposure time")
     hst_results.rename_column("sci_pep_id", "PID")
-    #hst_results.rename_column("sci_central_wavelength", "central wavelength")
 
     jwst_results.rename_column("instrume", "instrument")
     jwst_results.rename_column("opticalElements", "filters")
-    #jwst_results.rename_column("duration", "exposure time")
+    # jwst_results.rename_column("duration", "exposure time")
     jwst_results.rename_column("program", "PID")
 
-    combined_arr = astropy.table.vstack(
-        [hst_results, jwst_results], join_type="outer"
-    )
+    combined_arr = astropy.table.vstack([hst_results, jwst_results], join_type="outer")
 
     combined_arr = astropy.table.unique(combined_arr)
 
+    # assign observations to Galaxy object
 
-    #assign observations to Galaxy object
+    # return a .txt file with the output
+    filename = "MASTquery_output.txt"
+    with open(filename, "w") as file:
+        file.write("\t".join(combined_arr.colnames) + "\n")
+        for row in combined_arr:
+            file.write("\t".join(str(row[col]) for col in combined_arr.colnames) + "\n")
 
     return combined_arr
-
-
